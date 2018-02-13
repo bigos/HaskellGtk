9b0a2979bed9a31405bdffe8062211afbafc1762
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-13 12:18:47 +0000

    boxes example

---
+++ boxes/src/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified GI.Gtk as GI (init,
+                               main)
+import GI.Gtk (buttonNew,
+               boxNew,
+               containerAdd,
+               boxPackStart,
+               mainQuit,
+               onButtonClicked,
+               onWidgetDestroy,
+               setButtonLabel,
+               windowNew,
+               windowSetTitle,
+               widgetShowAll)
+import GI.Gtk.Enums
+       (WindowType(..), Orientation(..))
+
+main :: IO ()
+main = do
+  _ <- GI.init Nothing
+
+  win <- windowNew WindowTypeToplevel
+  windowSetTitle win "Hello World"
+
+  box <- boxNew OrientationHorizontal 10
+
+  btn1 <- buttonNew
+  setButtonLabel btn1 "Hello"
+  _ <- onButtonClicked btn1 (putStrLn "Hello")
+  boxPackStart box btn1 True True 0
+
+  btn2 <- buttonNew
+  setButtonLabel btn2 "Goodbye"
+  _ <- onButtonClicked btn2 (putStrLn "Goodbye")
+  boxPackStart box btn2 True True 0
+
+  containerAdd win box
+
+  _ <- onWidgetDestroy win mainQuit
+  widgetShowAll win
+  GI.main
